cd346cc0d52c4d88c28fa34dc8174fb21185dd4b
wfbreezee <wfbreezee@hotmail.com>
2019-09-24 08:53:49 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -3,17 +3,16 @@
  
 int main(void)
 {
-  CURL *curl;
-  CURLcode res;
- 
-  curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://myip.ipip.net/");
-    res = curl_easy_perform(curl);
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
-    curl_easy_cleanup(curl);
-  }
-  return 0;
+ CURL *curl;
+ CURLcode res;
+ curl = curl_easy_init();
+ if(curl) {
+  curl_easy_setopt(curl, CURLOPT_URL, "http://myip.ipip.net/");
+  res = curl_easy_perform(curl);
+  if(res != CURLE_OK)
+   fprintf(stderr, "curl_easy_perform() failed: %s\n",
+   curl_easy_strerror(res));
+   curl_easy_cleanup(curl);
+ }
+ return 0;
 }
